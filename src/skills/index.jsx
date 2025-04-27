ea4e9f9e4149231854e0ff7319bab8a34a0bb5c4
JamirKhan7 <jamirkhan007@gmail.com>
2025-04-27 13:45:32 +0530

    Skills section ui

--- src/skills/index.jsx
+++ src/skills/index.jsx
@@ -5,14 +5,15 @@ import './skills.scss';
 const skills = [
   { title: 'Ruby on Rails', logo: '/skills/ruby_on_rails.svg', logo_class: 'skills-ruby-on-rails-logo' },
   { title: 'React', logo: '/skills/react.svg' },
+  { title: 'JavaScript (ES6+)', logo: '/skills/javascript.svg' },
   { title: 'MySQL', logo: '/skills/mysql.svg' },
-  { title: 'JavaScript (ES6+)' },
-  { title: 'HTML5' },
-  { title: 'CSS3' },
-  { title: 'SCSS' },
-  { title: 'Bootstrap' },
-  { title: 'Git' },
-  { title: 'Visual Studio Code' }
+  { title: 'HTML5', logo: '/skills/html5.svg' },
+  { title: 'CSS3', logo: '/skills/css3.svg' },
+  { title: 'SASS', logo: '/skills/sass.svg' },
+  { title: 'Bootstrap', logo: '/skills/bootstrap.svg' },
+  { title: 'Git', logo: '/skills/git.svg' },
+  { title: 'Visual Studio Code', logo: '/skills/vscode.svg' },
+  { title: 'NodeJS', logo: '/skills/nodejs.svg' }
 ];
 
 const Skills = () => {
@@ -23,10 +24,10 @@ const Skills = () => {
         <p className="text-body-secondary mb-5">The skills, tools and technologies I use:</p>
         <Row className="g-3">
           {skills.map((skill, index) => (
-            <Col key={index} md={2}>
-              <Card className="h-100 border-0 rounded-3 p-3">
-                <img src={skill.logo} className={`skills-logo ${skill.logo_class}`} />
-                <Card.Title className='h6 mb-0'>{skill.title}</Card.Title>
+            <Col key={index} md={3}>
+              <Card className="h-100 border-0 rounded-3 p-3 flex-row align-items-center">
+                <img src={skill.logo} className={`skills-logo ${skill.logo_class || ''}`} />
+                <Card.Title className='mb-0 ms-3 base-font-size fw-normal'>{skill.title}</Card.Title>
               </Card>
             </Col>
           ))}
